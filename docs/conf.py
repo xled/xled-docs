5496753a6173d1b7c829f65a759b3a7c504e1c9f
Pavol Babinčák <scroolik@gmail.com>
2018-01-02 00:50:39 +0100

    Improve index docs

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ copyright = u"2017, Pavol Babinčák"
 # the built documents.
 #
 # The short X.Y version.
-version = '1.99.24.3'
+version = '1.99.24.4'
 # The full version, including alpha/beta/rc tags.
 release = version
 
